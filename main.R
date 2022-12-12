1dc966ec94d242fd753fe7f49aa3a60f97ae1fa8
Tasnim-20 <103363242+Tasnim-20@users.noreply.github.com>
2022-12-12 11:12:44 +0100

    Add files via upload

--- main.R
+++ main.R
@@ -1,3 +1,4 @@
+#Les library qu'on aura besoin 
 install.packages("terra")
 library(dplyr)
 library(maptools)
@@ -8,25 +9,37 @@ library(leaflet)
 library(raster)
 library(htmlwidgets)
 library(maptools)
-donnees <- read.csv("E:\\Tasnim chiba\\ing1\\analyse de donnees\\cartographie\\data.csv", header = TRUE, sep = ",", encoding = "latin1")
+#lecture des données à partir d'un fichier csv
+donnees <- read.csv("data.csv", header = TRUE, sep = ",", encoding = "latin1")
+
+#verification des dimensions de notre données
 dim(donnees)
+
+#chagement des emplacements de chaque gouvernorat
 tun <- getData("GADM", country="TUN", level=1)
+
+#projection des données sur notre map
 tun$nb <- donnees$nb
-pal <- colorQuantile("red", NULL, n = 5)
+
+#déterminer le nom et le nombre selon chaque gouvernorate en cliquant sur la gouvernorat
 polygon_popup <- paste0("<strong>Name: </strong>", donnees$city, "<br>",
                         "<strong>Indicator: </strong>", round(donnees$nb,2))
 
-
+#choit des couleur de la carte pour mettre en valeur la variation
 pal<-colorNumeric(
   palette = c("#ffff00","#ff0000"),
   domain = tun$nb
 )
+
+#détérminaison du titre
 title_ = "<div style='text-align: center; font-weight: bold;'>Nombre de naissance en Tunisie (2021)</div>"
 
 map = leaflet() %>% 
   setView(9.5, 34,zoom = 6) %>% 
   addControl(title_, position = "topleft") %>%
   addPolygons(data = tun,fillColor= ~pal(nb),fillOpacity = 0.4,weight = 2,color = "white",popup = polygon_popup)%>%
+  #ajout de la légende
 addLegend("bottomright", pal=pal, values=donnees$nb, title="Nombre de naissances selon lieu d'accouchement", opacity = 1)
 
+#affichage de la map
 map
